57994db2fb108f729c8c7592fba60e1a67464b04
djbobbo <bohous.steier@seznam.cz>
2022-01-18 13:24:15 +0100

    Ve škole teplomer

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 /*#include "delay.h"*/
 #include <stdio.h>*/
 #include "../lib/uart.c"*/
+#include "spse_stm8.h"
 
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
@@ -34,13 +35,38 @@ int main(void)
     setup();
     init_uart();
 
+    // na pinech/vstupech ADC_IN2 (PB2) a ADC_IN3 (PB3) vypneme vstupní buffer
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL4, DISABLE);
+    ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL5, DISABLE);
+
+    // při inicializaci volíme frekvenci AD převodníku mezi 1-4MHz při 3.3V
+    // mezi 1-6MHz při 5V napájení
+    // nastavíme clock pro ADC (16MHz / 4 = 4MHz)
+    ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
+
+    // volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
+    ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
+
+    // nasatvíme multiplexer na některý ze vstupních kanálů
+    ADC2_Select_Channel(ADC2_CHANNEL_4);
+    // rozběhneme AD převodník
+    ADC2_Cmd(ENABLE);
+    // počkáme než se AD převodník rozběhne (~7us)
+    ADC2_Startup_Wait();
+    
+    uint16_t ADCx;
+    uint32_t voltage;
+
+    
     while (1) {
 
-        if (milis() - time > 333 && BTN_PUSH) {
+        if (milis() - time > 1111) {
             LED_TOGG; 
             time = milis();
-
-            printf("Ahoj\n");
+            ADCx = ADC_get(ADC2_CHANNEL_4);
+            printf("%d\n", ADCx);
+            voltage = (uint32_t)3300 * ADCx + 512 /1024;
+            printf("R = %d U = %d\n", ADCx, voltage);
         }
     
 
